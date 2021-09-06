916fac7951b389a741cb34711553df553d5f3b89
GinMu <gin.musheng@gmail.com>
2021-09-06 10:21:45 +0800

    generate doc

---
+++ docs/assets/js/search.js
@@ -0,0 +1,296 @@
+window.searchData = {
+  kinds: { 128: "Class", 256: "Interface", 512: "Constructor", 1024: "Property", 2048: "Method" },
+  rows: [
+    { id: 0, kind: 256, name: "CertOptions", url: "interfaces/CertOptions.html", classes: "tsd-kind-interface" },
+    {
+      id: 1,
+      kind: 1024,
+      name: "senderSecret",
+      url: "interfaces/CertOptions.html#senderSecret",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "CertOptions"
+    },
+    {
+      id: 2,
+      kind: 1024,
+      name: "receiverAddress",
+      url: "interfaces/CertOptions.html#receiverAddress",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "CertOptions"
+    },
+    {
+      id: 3,
+      kind: 1024,
+      name: "token",
+      url: "interfaces/CertOptions.html#token",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "CertOptions"
+    },
+    {
+      id: 4,
+      kind: 1024,
+      name: "amount",
+      url: "interfaces/CertOptions.html#amount",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "CertOptions"
+    },
+    {
+      id: 5,
+      kind: 1024,
+      name: "issuer",
+      url: "interfaces/CertOptions.html#issuer",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "CertOptions"
+    },
+    {
+      id: 6,
+      kind: 1024,
+      name: "nodes",
+      url: "interfaces/CertOptions.html#nodes",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "CertOptions"
+    },
+    {
+      id: 7,
+      kind: 1024,
+      name: "chain",
+      url: "interfaces/CertOptions.html#chain",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "CertOptions"
+    },
+    { id: 8, kind: 256, name: "ICert", url: "interfaces/ICert.html", classes: "tsd-kind-interface" },
+    {
+      id: 9,
+      kind: 1024,
+      name: "cid",
+      url: "interfaces/ICert.html#cid",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "ICert"
+    },
+    {
+      id: 10,
+      kind: 1024,
+      name: "txHash",
+      url: "interfaces/ICert.html#txHash",
+      classes: "tsd-kind-property tsd-parent-kind-interface",
+      parent: "ICert"
+    },
+    { id: 11, kind: 128, name: "default", url: "classes/default.html", classes: "tsd-kind-class" },
+    {
+      id: 12,
+      kind: 2048,
+      name: "fetchNodes",
+      url: "classes/default.html#fetchNodes",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-static",
+      parent: "default"
+    },
+    {
+      id: 13,
+      kind: 2048,
+      name: "hex2str",
+      url: "classes/default.html#hex2str",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-static",
+      parent: "default"
+    },
+    {
+      id: 14,
+      kind: 512,
+      name: "constructor",
+      url: "classes/default.html#constructor",
+      classes: "tsd-kind-constructor tsd-parent-kind-class tsd-is-overwrite",
+      parent: "default"
+    },
+    {
+      id: 15,
+      kind: 1024,
+      name: "senderSecret",
+      url: "classes/default.html#senderSecret",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+      parent: "default"
+    },
+    {
+      id: 16,
+      kind: 1024,
+      name: "senderAddress",
+      url: "classes/default.html#senderAddress",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+      parent: "default"
+    },
+    {
+      id: 17,
+      kind: 1024,
+      name: "receiverAddress",
+      url: "classes/default.html#receiverAddress",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+      parent: "default"
+    },
+    {
+      id: 18,
+      kind: 1024,
+      name: "token",
+      url: "classes/default.html#token",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+      parent: "default"
+    },
+    {
+      id: 19,
+      kind: 1024,
+      name: "amount",
+      url: "classes/default.html#amount",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+      parent: "default"
+    },
+    {
+      id: 20,
+      kind: 1024,
+      name: "issuer",
+      url: "classes/default.html#issuer",
+      classes: "tsd-kind-property tsd-parent-kind-class tsd-is-private",
+      parent: "default"
+    },
+    {
+      id: 21,
+      kind: 2048,
+      name: "isSuccess",
+      url: "classes/default.html#isSuccess",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-protected",
+      parent: "default"
+    },
+    {
+      id: 22,
+      kind: 2048,
+      name: "saveCert",
+      url: "classes/default.html#saveCert",
+      classes: "tsd-kind-method tsd-parent-kind-class tsd-is-protected",
+      parent: "default"
+    },
+    {
+      id: 23,
+      kind: 2048,
+      name: "saveHashCert",
+      url: "classes/default.html#saveHashCert",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "default"
+    },
+    {
+      id: 24,
+      kind: 2048,
+      name: "saveBufferCert",
+      url: "classes/default.html#saveBufferCert",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "default"
+    },
+    {
+      id: 25,
+      kind: 2048,
+      name: "checkCert",
+      url: "classes/default.html#checkCert",
+      classes: "tsd-kind-method tsd-parent-kind-class",
+      parent: "default"
+    }
+  ],
+  index: {
+    version: "2.3.9",
+    fields: ["name", "parent"],
+    fieldVectors: [
+      ["name/0", [0, 11.558]],
+      ["parent/0", []],
+      ["name/1", [1, 23.795]],
+      ["parent/1", [0, 1.097]],
+      ["name/2", [2, 23.795]],
+      ["parent/2", [0, 1.097]],
+      ["name/3", [3, 23.795]],
+      ["parent/3", [0, 1.097]],
+      ["name/4", [4, 23.795]],
+      ["parent/4", [0, 1.097]],
+      ["name/5", [5, 23.795]],
+      ["parent/5", [0, 1.097]],
+      ["name/6", [6, 28.904]],
+      ["parent/6", [0, 1.097]],
+      ["name/7", [7, 28.904]],
+      ["parent/7", [0, 1.097]],
+      ["name/8", [8, 20.431]],
+      ["parent/8", []],
+      ["name/9", [9, 28.904]],
+      ["parent/9", [8, 1.94]],
+      ["name/10", [10, 28.904]],
+      ["parent/10", [8, 1.94]],
+      ["name/11", [11, 5.55]],
+      ["parent/11", []],
+      ["name/12", [12, 28.904]],
+      ["parent/12", [11, 0.527]],
+      ["name/13", [13, 28.904]],
+      ["parent/13", [11, 0.527]],
+      ["name/14", [14, 28.904]],
+      ["parent/14", [11, 0.527]],
+      ["name/15", [1, 23.795]],
+      ["parent/15", [11, 0.527]],
+      ["name/16", [15, 28.904]],
+      ["parent/16", [11, 0.527]],
+      ["name/17", [2, 23.795]],
+      ["parent/17", [11, 0.527]],
+      ["name/18", [3, 23.795]],
+      ["parent/18", [11, 0.527]],
+      ["name/19", [4, 23.795]],
+      ["parent/19", [11, 0.527]],
+      ["name/20", [5, 23.795]],
+      ["parent/20", [11, 0.527]],
+      ["name/21", [16, 28.904]],
+      ["parent/21", [11, 0.527]],
+      ["name/22", [17, 28.904]],
+      ["parent/22", [11, 0.527]],
+      ["name/23", [18, 28.904]],
+      ["parent/23", [11, 0.527]],
+      ["name/24", [19, 28.904]],
+      ["parent/24", [11, 0.527]],
+      ["name/25", [20, 28.904]],
+      ["parent/25", [11, 0.527]]
+    ],
+    invertedIndex: [
+      ["amount", { _index: 4, name: { 4: {}, 19: {} }, parent: {} }],
+      ["certoptions", { _index: 0, name: { 0: {} }, parent: { 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {} } }],
+      ["chain", { _index: 7, name: { 7: {} }, parent: {} }],
+      ["checkcert", { _index: 20, name: { 25: {} }, parent: {} }],
+      ["cid", { _index: 9, name: { 9: {} }, parent: {} }],
+      ["constructor", { _index: 14, name: { 14: {} }, parent: {} }],
+      [
+        "default",
+        {
+          _index: 11,
+          name: { 11: {} },
+          parent: {
+            12: {},
+            13: {},
+            14: {},
+            15: {},
+            16: {},
+            17: {},
+            18: {},
+            19: {},
+            20: {},
+            21: {},
+            22: {},
+            23: {},
+            24: {},
+            25: {}
+          }
+        }
+      ],
+      ["fetchnodes", { _index: 12, name: { 12: {} }, parent: {} }],
+      ["hex2str", { _index: 13, name: { 13: {} }, parent: {} }],
+      ["icert", { _index: 8, name: { 8: {} }, parent: { 9: {}, 10: {} } }],
+      ["issuccess", { _index: 16, name: { 21: {} }, parent: {} }],
+      ["issuer", { _index: 5, name: { 5: {}, 20: {} }, parent: {} }],
+      ["nodes", { _index: 6, name: { 6: {} }, parent: {} }],
+      ["receiveraddress", { _index: 2, name: { 2: {}, 17: {} }, parent: {} }],
+      ["savebuffercert", { _index: 19, name: { 24: {} }, parent: {} }],
+      ["savecert", { _index: 17, name: { 22: {} }, parent: {} }],
+      ["savehashcert", { _index: 18, name: { 23: {} }, parent: {} }],
+      ["senderaddress", { _index: 15, name: { 16: {} }, parent: {} }],
+      ["sendersecret", { _index: 1, name: { 1: {}, 15: {} }, parent: {} }],
+      ["token", { _index: 3, name: { 3: {}, 18: {} }, parent: {} }],
+      ["txhash", { _index: 10, name: { 10: {} }, parent: {} }]
+    ],
+    pipeline: []
+  }
+};
